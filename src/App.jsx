ec7c1f56d0c0966215aefc289d2c1f13313520a4
Aditya Mishra <adityakumarsmishra@gmail.com>
2026-02-13 11:09:17 +0530

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   src/App.jsx #	modified:   src/main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import "./App.css";
 import Home from "./Pages/Home/Home";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Blogs from "./Pages/Blogs/Blogs";
+
 function App() {
   return (
     <div className="app">
-      <Navbar></Navbar>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/Blogs" element={<Blogs></Blogs>}></Route>
-        </Routes>
-      </HashRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Blogs" element={<Blogs />} />
+      </Routes>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/blog-app">
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
 );
