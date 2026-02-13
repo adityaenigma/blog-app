67df53ac18c5bae2e9733b6aa78392b4a072af3a
Aditya Mishra <adityakumarsmishra@gmail.com>
2026-02-13 10:55:59 +0530

    commit for package json

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import "./App.css";
 import Home from "./Pages/Home/Home";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Blogs from "./Pages/Blogs/Blogs";
 function App() {
   return (
     <div className="app">
       <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/Blogs" element={<Blogs></Blogs>}></Route>
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/Blogs" element={<Blogs></Blogs>}></Route>
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
